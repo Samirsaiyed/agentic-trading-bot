87e9daad74c0b14f22f4eafcd9690fed2f2662c7
samirsaiyed256 <samirsaiyed256@gmail.com>
2025-07-14 04:31:50 +0000

    Updated skeleton and add data

---
+++ data_ingestion/ingestion_pipeline.py
@@ -0,0 +1,15 @@
+
+
+
+class DataIngestion:
+    def __inin(self):
+        pass
+
+    def _load_env_vairables(self):
+        pass
+
+    def load_document(self):
+        pass
+
+    def store_in_vector_db(self):
+        pass
